6c9a100f7b0ba3b33eeaeadf457cfcc00fd6af75
vasanth <vasanthkirshvasanthkirsh@gmail.com>
2024-09-02 19:03:24 +0530

    remove the unwanted home

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -72,46 +72,4 @@ class NewConversationScreen extends GetView {
   }
 }
 
-class NewConversationScreen extends GetView<HomeController> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('New conversation'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            TextField(
-              decoration: InputDecoration(
-                hintText: 'Type a name, phone number, or email',
-                border: UnderlineInputBorder(),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 20.0),
-              child: ListTile(
-                leading: Icon(Icons.group_add, color: Colors.blue),
-                title: Text('Create group'),
-                onTap: () {
-                },
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
 
-void main() {
-  runApp(
-    GetMaterialApp(
-      home: HomeScreen(),
-      initialBinding: BindingsBuilder(() {
-        Get.put(HomeController());
-      }),
-    ),
-  );
-}
